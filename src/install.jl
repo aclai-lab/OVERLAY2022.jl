ce22103ce8a19487b927518451878f107c406f9c
mauro-milella <milella_mauro@live.it>
2022-10-15 14:00:07 +0200

    Dependencies updated (ad-hoc SoleLogics branch)

--- src/install.jl
+++ src/install.jl
@@ -9,7 +9,7 @@ using Pkg
 Pkg.add(url="https://github.com/aclai-lab/SoleTraits.jl")
 Pkg.add(url="https://github.com/aclai-lab/SoleAlphabets.jl", rev="dev")
 Pkg.add(url="https://github.com/aclai-lab/SoleWorlds.jl", rev="worlds/mauro")
-Pkg.add(url="https://github.com/aclai-lab/SoleLogics.jl", rev="refactoring/mauro")
+Pkg.add(url="https://github.com/aclai-lab/SoleLogics.jl", rev="overlay2022/mauro")
 Pkg.add(url="https://github.com/aclai-lab/SoleModelChecking.jl", rev="mfmm/mauro")
 
 # Registered packages
