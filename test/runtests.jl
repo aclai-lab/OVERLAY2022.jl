ba611fa9e97d9c512fe53c2214a17c0f6c49bdc4
mauro-milella <milella_mauro@live.it>
2022-11-21 21:43:09 +0100

    Manifest.toml now refers to specific SOLE repo tags. Unit test comment clarified.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,5 @@ using OVERLAY2022
 using Test
 
 @testset "OVERLAY2022.jl" begin
-    # Unit tests are currently placed in different SOLE modules.
-    # This reference is going to be updated after tidying SOLE structure.
+    # Unit tests are placed in the v0.1 build of SOLE dependencies.
 end
